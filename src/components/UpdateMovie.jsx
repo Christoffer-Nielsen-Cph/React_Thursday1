0d6062733503169e28b2477dae681b5aa26151d3
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-10-27 11:59:32 +0200

    show or hide

--- src/components/UpdateMovie.jsx
+++ src/components/UpdateMovie.jsx
@@ -2,7 +2,14 @@ import React, {useState, useEffect} from 'react';
 import AddMovie from "./AddMovie.jsx";
 const updateMovie = (props) => {
     const [updatedMovie, setUpdatedMovie] = useState({id:'',title:'',year:'',rating:'',genre:''});
-
+    const [hidden,setHidden] = useState(true)
+    const btnText = () => {
+        if(hidden){
+            return "Update movie"
+        } else{
+            return "Close"
+        }
+    }
     const update = (evt) => {
         const value = evt.target.value;
         const propertyName = evt.target.id;
@@ -35,19 +42,20 @@ const updateMovie = (props) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label className='labelUpdateMovie'>id:</label>
-                <input className='inputUpdateMovie' type="Number" id="id" value={updatedMovie.id} onChange={update}/>
-                <label className='labelUpdateMovie'>Title:</label>
-                <input className='inputUpdateMovie' type="text" id="title" value={updatedMovie.title} onChange={update}/>
-                <label className='labelUpdateMovie'>Year:</label>
-                <input className='inputUpdateMovie'  type="text" id="year" value={updatedMovie.year} onChange={update}/>
-                <label className='labelUpdateMovie'>Rating:</label>
-                <input className='inputUpdateMovie'  type="text" id="rating" value={updatedMovie.rating} onChange={update}/>
-                <label className='labelUpdateMovie'>Genre:</label>
-                <input className='inputUpdateMovie'  type="text" id="genre" value={updatedMovie.genre} onChange={update}/>
+            <button onClick={()=>setHidden(s => !s)}>{btnText()}</button>
+            {!hidden ? <form onSubmit={handleSubmit}>
+                <label className='labelNewMovie'>Id:</label>
+                <input className='inputNewMovie' type="number" id="id" value={updatedMovie.id} onChange={update}/>
+                <label className='labelNewMovie'>Title:</label>
+                <input className='inputNewMovie' type="text" id="title" value={updatedMovie.title} onChange={update}/>
+                <label className='labelNewMovie'>Year:</label>
+                <input className='inputNewMovie'  type="text" id="year" value={updatedMovie.year} onChange={update}/>
+                <label className='labelNewMovie'>Rating:</label>
+                <input className='inputNewMovie'  type="text" id="rating" value={updatedMovie.rating} onChange={update}/>
+                <label className='labelNewMovie'>Genre:</label>
+                <input className='inputNewMovie'  type="text" id="genre" value={updatedMovie.genre} onChange={update}/>
                 <input type="submit" value="Update"/>
-            </form>
+            </form> : null}
         </>
     );
 }
